999031bc83ca2a55ee980e918cf7d91f062c6c71
Isaac Blanc <nuclearhands2001@gmail.com>
2023-02-10 10:04:25 -0500

    document on script

--- src/stories/ModulesMenu.stories.tsx
+++
@@ -1,7 +0,0 @@
-import ModulesMenu from "../components/ModulesMenu";
-
-export default { title: "Example/ModulesMenu", component: ModulesMenu };
-
-export const Info = () => (
-    <ModulesMenu modules={[]}></ModulesMenu> 
-);
